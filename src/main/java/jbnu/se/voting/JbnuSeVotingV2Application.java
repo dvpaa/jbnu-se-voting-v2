f512409e4789946739feea602511f673776b9e68
geonu <kgo000926@gmail.com>
2024-02-06 21:23:19 +0900

    init project

---
+++ src/main/java/jbnu/se/voting/JbnuSeVotingV2Application.java
@@ -0,0 +1,13 @@
+package jbnu.se.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbnuSeVotingV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbnuSeVotingV2Application.class, args);
+	}
+
+}
