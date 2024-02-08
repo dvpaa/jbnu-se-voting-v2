ffb3a6bfb305b41906d9b5d393f6c49058d524d5
geonu <kgo000926@gmail.com>
2024-02-08 16:18:46 +0900

    refactor: set embedded type immutable

--- src/main/java/jbnu/se/api/domain/Member.java
+++ src/main/java/jbnu/se/api/domain/Member.java
@@ -2,12 +2,14 @@ package jbnu.se.api.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Column(nullable = false)
--- src/main/java/jbnu/se/api/domain/Period.java
+++ src/main/java/jbnu/se/api/domain/Period.java
@@ -2,14 +2,16 @@ package jbnu.se.api.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Embeddable
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Period {
 
     @Column(nullable = false)
