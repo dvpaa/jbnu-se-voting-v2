773464b04502635383a43832e42d81145b53cf8c
geonu <kgo000926@gmail.com>
2024-02-13 16:52:57 +0900

    feat: setting auth profile

---
+++ src/main/java/jbnu/se/api/config/AppConfig.java
@@ -0,0 +1,31 @@
+package jbnu.se.api.config;
+
+import jbnu.se.api.security.JwtUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public AuthProperties authProperties() {
+        return new AuthProperties();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(authProperties().getOasisApi().getUrl())
+                .defaultHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil(authProperties());
+    }
+}
