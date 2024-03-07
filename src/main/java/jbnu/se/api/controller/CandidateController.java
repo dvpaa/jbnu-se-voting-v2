39744db9608944f363ed99baa9a694949498169f
geonu <kgo000926@gmail.com>
2024-03-07 13:34:29 +0900

    test: candidate

--- src/main/java/jbnu/se/api/controller/CandidateController.java
+++ src/main/java/jbnu/se/api/controller/CandidateController.java
@@ -3,10 +3,8 @@ package jbnu.se.api.controller;
 import jbnu.se.api.request.CandidatePair;
 import jbnu.se.api.service.CandidateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +16,7 @@ public class CandidateController {
     private final CandidateService candidateService;
 
     @PostMapping("/candidates")
+    @ResponseStatus(HttpStatus.CREATED)
     public void registerCandidate(@RequestBody List<CandidatePair> candidatePairs) {
         candidateService.registerCandidate(candidatePairs);
     }
